b24be94b068188054f4094855da46911463559f9
itscomputers <its_computers@protonmail.com>
2019-12-03 23:05:28 -0700

    consolidate day 1 tests

--- test/day01_test.rb
+++ test/day01_test.rb
@@ -3,16 +3,23 @@ require_relative "../lib/day01"
 
 class TestDay01 < MiniTest::Test
   def setup
-    @result_one = 3425624
-    @result_two = 5135558
-    @solver = Day01.new()
+    @testing = {
+      [12] => [2, 2],
+      [14] => [2, 2],
+      [1969] => [654, 966],
+      [100756] => [33583, 50346]
+    }
   end
 
   def test_result_one
-    assert_equal @solver.run_one, @result_one
+    @testing.each do |test_data, results|
+      assert_equal Day01.new(test_data).run_one, results.first
+    end
   end
 
   def test_result_two
-    assert_equal @solver.run_two, @result_two
+    @testing.each do |test_data, results|
+      assert_equal Day01.new(test_data).run_two, results.last
+    end
   end
 end
