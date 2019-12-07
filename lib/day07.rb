12b8c1b9e585db0646d5214d24d68d3bc5689be0
itscomputers <its_computers@protonmail.com>
2019-12-07 10:12:44 -0700

    solution to day 7

---
+++ lib/day07.rb
@@ -0,0 +1,44 @@
+require_relative 'solver'
+require_relative 'intcode_computer'
+
+class Day07 < Solver
+  def get_data
+    File.read(file_name).split(',').map(&:to_i)
+  end
+
+  def run_one
+    (0..4).to_a.permutation(5).map(&method(:from_sequence)).max
+  end
+
+  def run_two
+    (5..9).to_a.permutation(5).map(&method(:from_sequence_with_feedback)).max
+  end
+
+  def from_sequence(inputs)
+    default_input = 0
+    inputs.map do |input|
+      default_input = IntcodeComputer.new(
+        program: data,
+        inputs: [input],
+        default_input: default_input
+      ).run.output.last
+    end
+    default_input
+  end
+
+  def from_sequence_with_feedback(inputs)
+    default_input = 0
+    computers = inputs.map do |s|
+      IntcodeComputer.new(program: data, inputs: [s], default_input: default_input)
+    end
+
+    idx = 0
+    while computers.map(&:instruction_op_id).uniq != [99]
+      computer = computers[idx % 5]
+      computer.default_input = default_input
+      default_input = computer.advance_to_next_output.output.last
+      idx = idx + 1
+    end
+    default_input
+  end
+end
